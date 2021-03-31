a63908a9a9bd43bb9203666fc367b0803ecebf3b
adamel2 <elddawork@gmail.com>
2021-03-31 12:57:32 +0300

    init

---
+++ src/test/java/com/bio/v1/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bio.v1.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
